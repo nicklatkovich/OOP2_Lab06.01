bdc7d3b936a27dbaee88cb0addc6feb9203783d3
NickLatkovich <nicklatkovich@gmail.com>
2017-04-27 08:34:35 +0300

    Addition to ViewList

--- OOP_Lab06.01/Form1.Designer.cs
+++ OOP_Lab06.01/Form1.Designer.cs
@@ -23,12 +23,58 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent( ) {
-            this.components = new System.ComponentModel.Container( );
+            this.ListView = new System.Windows.Forms.ListView();
+            this.TextInput = new System.Windows.Forms.TextBox();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListView
+            // 
+            this.ListView.Location = new System.Drawing.Point(12, 12);
+            this.ListView.Name = "ListView";
+            this.ListView.Size = new System.Drawing.Size(220, 355);
+            this.ListView.TabIndex = 0;
+            this.ListView.UseCompatibleStateImageBehavior = false;
+            this.ListView.SelectedIndexChanged += new System.EventHandler(this.ListView_SelectedIndexChanged);
+            // 
+            // TextInput
+            // 
+            this.TextInput.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextInput.Location = new System.Drawing.Point(238, 12);
+            this.TextInput.Name = "TextInput";
+            this.TextInput.Size = new System.Drawing.Size(188, 32);
+            this.TextInput.TabIndex = 1;
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Location = new System.Drawing.Point(296, 50);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAdd.TabIndex = 2;
+            this.ButtonAdd.Text = "Add";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 379);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.TextInput);
+            this.Controls.Add(this.ListView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView ListView;
+        private System.Windows.Forms.TextBox TextInput;
+        private System.Windows.Forms.Button ButtonAdd;
     }
 }
 
--- OOP_Lab06.01/Form1.cs
+++ OOP_Lab06.01/Form1.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using OOP_Lab04;
 
 namespace OOP_Lab06._01 {
     public partial class Form1 : Form {
+
+        public CollectionType<string> CT = new CollectionType<string>( );
+
         public Form1( ) {
             InitializeComponent( );
+        }
+
+        private void ButtonAdd_Click(Object sender, EventArgs e) {
+            if (CT.Contains(this.TextInput.Text)) {
+                MessageBox.Show("This value is already in the set");
+            } else {
+                CT.Add(this.TextInput.Text);
+                ListView.Items.Add(this.TextInput.Text);
+                this.TextInput.Text = "";
+            }
+        }
+
+        public void ListViewUpdate( ) {
+        }
+
+        private void ListView_SelectedIndexChanged(Object sender, EventArgs e) {
+
         }
     }
 }
